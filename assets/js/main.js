3534d3f0cf16baca0bd703e240da2435a49ba66c
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-20 17:26:48 +0100

    add fix if else click function

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,58 +41,75 @@ buttonStart.addEventListener("click", function(){
     //creo un array vuoto per inserire i numeri casuali
     const bombCell = []
     
-    console.log(bombCell);
+    
 
     for (let index = 1; index <= 16; index++) {
     //creo una variabile che ci restituisce un numero randomico tra 1 e 16
-        const bombNumber = Math.ceil(Math.random() * 100);
+        const bombNumber = Math.floor(Math.random() * 100)+1;
 
-        if (bombCell !== nMax) {
+        if (bombNumber !== nMax) {
 
             bombCell.push(bombNumber);
             
         } 
         
-        
     }
+
+    
   // seleziono una cella che ha classe cell e active
 
     const cellEl = document.querySelectorAll(".cell")
     
-    // aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
-
-
     
 
+
+    //creo un for loop  
     for (let j = 0; j < cellEl.length; j++) {
         
         const thisCell = cellEl[j];
         console.log(thisCell)
+        console.log(bombCell.includes(j + 1))
 
-        if (thisCell == bombCell) {
-            
-            thisCell.addEventListener("click", function() {
-                thisCell.classList.toggle("bg_red")
+      
+        // aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
+
+        thisCell.addEventListener("click", function() {
+
+
+            if (bombCell.includes(j + 1) == true) {
+
+                cellEl[j].classList.toggle("bg_red")
                 // this.classList.add("active")
                 console.log(`You're fu**ed`)
-                
-            })
 
-        } else {
+                alert(`You're fu**ed`)
+        
+            } else if (bombCell.includes(j + 1) == false) {
             
-            thisCell.addEventListener("click", function() {
-                thisCell.classList.toggle("bg_lightblue")
+                cellEl[j].classList.toggle("bg_lightblue")
                 // this.classList.add("active")
-                console.log(`You clicked number ${j + 1}`)
-                
-            })
+                console.log(`You clicked number ${j}`)
+            
+            }
 
-        }
+
+            
+            
+        })
+
+       
+            
+            
+                
+                
+            
+
+       
         
 
     };
 
-   
+    console.log(bombCell);
 
     
 });
